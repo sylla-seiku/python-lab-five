b7eadbf59a91780d59abce14e40dfee9f86a067a
sylla-seiku <seikusylla@gmail.com>
2023-09-11 15:46:39 -0400

    learning About Keyword and Positional Arguments.

--- functions.py
+++ functions.py
@@ -1,3 +1,17 @@
+#learning About Keyword and Positional Arguments.
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome abpard')
+
+
+print("Start")
+greet_user("Sillah", "Seiku") # " 'Seiku' 'Sillah' here is considered a positional argument". so the position or order matters.
+greet_user(last_name="Sillah", first_name="Seiku") # this example here is known as a keyword argument ans the positioning and order doesnt matter.
+#if one is going to mix keyword and positional argumeants together it is best to use the positional followed by the keyword argument.
+print("Finish")
+
+
+'''
 #Learning about Parameters
 def greet_user(name): #the name function here is a prameter
     print(f'Hi {name}!')
@@ -28,7 +42,7 @@ def greet_user(name):
 greet_user("")
 
 
-'''
+
 #Learning About Functions.
 def greet_user():
     print('Hi there!')
