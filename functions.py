4e58408518ad8a0dc004e1ee353f28a1e4c0f016
sylla-seiku <seikusylla@gmail.com>
2023-09-11 15:28:13 -0400

    Learning About Parameters.

--- functions.py
+++ functions.py
@@ -1,3 +1,34 @@
+#Learning about Parameters
+def greet_user(name): #the name function here is a prameter
+    print(f'Hi {name}!')
+    print('Welcome abpard')
+
+
+print("Start")
+greet_user("Seiku") # "Seiku" right here is an argument.
+greet_user("Beyage")
+print("Finish")
+
+#diffirent example
+def greet_user(first_name, last_name): #the name function here is a prameter
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome abpard')
+
+
+print("Start")
+greet_user("Seiku", "Sillah") # "Seiku Sillah" right here is an argument.
+print("Finish")
+
+#practice inputting name with the great messege.
+def greet_user(name):
+    name = input("=>")
+    print(f'Hi {name}!')
+    print('Welcome abpard')
+
+greet_user("")
+
+
+'''
 #Learning About Functions.
 def greet_user():
     print('Hi there!')
@@ -7,4 +38,4 @@ def greet_user():
 print("Start")
 greet_user()
 print("Finish")
-
+'''
