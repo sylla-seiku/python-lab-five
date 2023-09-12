ba07c9fc115f004f31bf21b92fd96c4f6032730b
sylla-seiku <seikusylla@gmail.com>
2023-09-12 08:57:06 -0400

    Learning About Classes.

--- more.py
+++ more.py
@@ -8,7 +8,14 @@ class Point:
 
 
 point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.x)
 point1.draw()
+
+point2 = Point()
+point2.x = 1
+print(point2.x)
 
 #learning about comments 
 # I'm very familiar with comments so i didnot take any notes or write code on them.
